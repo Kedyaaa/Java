6bb0a41683e7045b21009827a87805b55d89e1a3
Kedyaaa <160596593+Kedyaaa@users.noreply.github.com>
2024-03-14 14:13:20 +0530

    Add files via upload

--- Test2.java
+++ Test2.java
@@ -1,22 +1,32 @@
-package com.mar4;
-class Student{
-	String name;
-	Student(){
-		name="unknown";
-		System.out.println(name);
-	}
-	Student(String n){
-		name=n;
-		System.out.println(name);
-	}
-}
+package com.mar12;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Collections;
 
 public class Test2 {
-	
 
 	public static void main(String[] args) {
-		Student s1=new Student();
-		Student s2=new Student("Sam Dao");
+		ArrayList<String>a1=new ArrayList<String>();
+		a1.add("Red");
+		a1.add("black");
+		a1.add("brown");
+		a1.add("white");
+		a1.add("orange");
+		a1.add("brown");
+		System.out.println(a1);
+		
+		System.out.println(a1.indexOf("brown"));
+		System.out.println(a1.lastIndexOf("brown"));
+		Collections.sort(a1);
+		System.out.println(a1);
+		
+		Iterator itr=a1.iterator();
+		
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+			
+		}
 
 	}
 
